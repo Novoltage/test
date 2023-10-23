3434a020329e4e5aecd5b8d0825ce211a73e718d
Kevin Bonifacio <kevin>
2023-10-22 23:05:49 -0400

    div creator done, grid needs work

--- Test/word-chop.js
+++ Test/word-chop.js
@@ -1,7 +1,7 @@
 /* 
-Win/lose condition fixed, set timeout implemented
+auto page reload fixed, create div(guess box) with id and class attached,css for div's guess box
 
-10/15
+10/22
 */
 
 guessWord = [''] ;
@@ -19,20 +19,19 @@ const Hint =inputElement2.value;
 for (let i = 0; i < Word.length; i++) {
 const a = Word.slice(i, (i+1));
 guessWord.push(a);
+const element = document.createElement('div'); //div creator
+element.id = `${i+1}`;
+element.className = 'letterBox';
+element.innerHTML = ' ';
+document.getElementById("guessBox").appendChild(element);
 }
 document.querySelector(".Print").innerHTML = Hint;
 document.getElementById("letters").classList.replace("off", "letters");
 document.getElementById("word").classList.replace("word", "off")
 }
-function wordDisplay(){
 
-}
 
-function play(){ //Starts Game
-wordGenerator();
-wordDisplay();
 
-}
 
 
 function reset(){
@@ -71,23 +70,35 @@ if(i === -1 && c ===0){
   alert(`wrong \n You have ${life} tries left`)
   
 }else{
-  console.log(guessWord[i])
-  wordChecker[i] = guessWord[i]
-  c++
+  //console.log(guessWord[i]);
+  wordChecker[i] = guessWord[i];
+  c++;
+  if(i>0){
+    console.log(`${i}`)
+  document.getElementById(`${i}`).innerHTML = `${guessWord[i]}`;//adds right guesses to guess box
+  }
 }
+document.getElementById(`${letter}`).classList.add("disable");
 
 if(wordChecker.toString() === guessWord.toString()){ //win condition
   document.querySelector(".Print").innerHTML = 'You win!';
   alert("You Win!")
   reset();
-  location.reload(true);
+  setTimeout(function(){
+    window.location.reload();
+ }, 3000);
+  //location.reload(true);
   
 
 }else if (life ===0){ //lose condition
   document.querySelector(".Print").innerHTML = 'You lose!';
   alert("You Lose!")
   reset();
-  location.reload(true)
+  setTimeout(function(){
+    window.location.reload();
+ }, 3000);
+  //location.reload(true)
 }
 }
+
 }
